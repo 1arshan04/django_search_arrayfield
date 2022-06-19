050589b38fc605108a0980d2fd4e8f6f805f4020
1arshan04 <1arshan@thetarzanway.com>
2022-06-19 20:41:01 +0530

    deleted old data

--- django_search_arrayfield/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "1.4.2"
-
-default_app_config = "django_search_arrayfield.apps.DjangoSearchArrayfieldConfig"
--- django_search_arrayfield/apps.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8
-from django.apps import AppConfig
-
-
-class DjangoSearchArrayfieldConfig(AppConfig):
-    name = 'django_search_arrayfield'
